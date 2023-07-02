b141c4d3f808f89f96c565f475ac4ec1249db059
Muhammad Tayyab Tahir Qureshi <m.tayyabtq@gmail.com>
2023-07-02 16:58:55 +0500

    3rd commit => created the sum function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello!!! I'm learning Git")
+def sum(a,b):
+    return int(a) + int(b)
+
+flag = True
+while (flag):
+    a = input("Enter first number: ")
+    b = input("Enter second number: ")
+    s = sum(a,b)
+    print(f"Sum = {s}")
+    e = input("If you want to exit press \'e\'")
+    if e=="e":
+        flag = False
+    continue
